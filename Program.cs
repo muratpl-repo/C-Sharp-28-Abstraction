683cac90ee363a312a4cbbbf1fcef8a3edca04ee
muratpala007 <muratpala007@gmail.com>
2022-04-19 18:30:43 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72DersAbstractSiniflarveMetotlar
+{
+    class Program
+    {
+        //Abstract: Soyut
+        abstract class Sekil    // Abstract metotlar sadece abstract siniflarda kullanilabilir
+                                // Türev sınıflarının doldurması zorlanır
+                                // Virtual'de ise isteğe bağlı override edilebilir..
+        {                       // Abstract siniflardan orneklemeler olusturulamaz. Sadece miras alinmasi icin olusturulur...
+            public abstract void SekilCiz();
+        }
+
+        class Cember : Sekil
+        {
+            public override void SekilCiz() //Abstract siniflari miras alinca mecburen methotlar, override yapmak gerekir...
+            {
+                Console.WriteLine("Cember ciziliyor...");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Cember c1 = new Cember();
+            c1.SekilCiz();
+        }
+    }
+}
